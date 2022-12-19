57ccfea6a49f9033e9cb2bd9ef28eb2e06e9a61b
Kira Lau <yuwinbizz@gmail.com>
2022-12-19 16:07:25 +0800

    fixed index + about

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+# ! load database into view
 from .models import Listing
 
 # Create your views here.
@@ -10,8 +12,15 @@ def index(request):
     # ! get all data from listing database
     listings = Listing.objects.all()
     # ! use context as  "global" variable to hold the dictionary rather than  _ variable value passing
+    paginator = Paginator(listings, 3)
+    # ! add paginatation
+    page = request.GET.get('page')
+    # API/LISTINGS/PAGE?=
+    # 係個 input method, 所以用 GET
+    paged_listings = paginator.get_page(page)
+
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
     return render(request, 'listings/listings.html', context)
 #  templates/listings/listings
